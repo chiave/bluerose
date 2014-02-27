ec0fd40ec26a0a2d972b44461853ea989984d45b
alphanumerix <konrad.mski@gmail.com>
2014-02-27 16:28:37 +0100

    Date.timezone fix

--- web/app.php
+++ web/app.php
@@ -3,6 +3,9 @@
 use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 
+
+date_default_timezone_set('Europe/Warsaw');
+
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 
 // Use APC for autoloading to improve performance.
